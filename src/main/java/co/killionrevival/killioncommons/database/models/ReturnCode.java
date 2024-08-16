1d7457fb0a607f3fc67e0fc3873d46f43b7e6e26
Matt Strenk <strenkml@users.noreply.github.com>
2024-08-15 22:42:08 -0400

    Add Truthy values to ReturnCode
    (cherry picked from commit 38ba1b8732db477dbbfc9286b2e12b7c6e03fcb4)

--- src/main/java/co/killionrevival/killioncommons/database/models/ReturnCode.java
+++ src/main/java/co/killionrevival/killioncommons/database/models/ReturnCode.java
@@ -4,8 +4,18 @@ package co.killionrevival.killioncommons.database.models;
  * Return code for sql query methods
  */
 public enum ReturnCode {
-    SUCCESS,
-    ALREADY_EXISTS,
-    NOT_EXIST,
-    FAILURE
+    SUCCESS(true),
+    ALREADY_EXISTS(false),
+    NOT_EXIST(false),
+    FAILURE(false);
+
+    private boolean truthy;
+
+    ReturnCode(boolean truthy) {
+        this.truthy = truthy;
+    }
+
+    public boolean isTruthy() {
+        return this.truthy;
+    }
 }
