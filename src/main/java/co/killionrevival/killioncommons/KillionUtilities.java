27b11ef4a16adc39d967e575b053d906efe8fdc5
Excaliburns <tutsandallthat@gmail.com>
2024-08-25 21:23:39 -0400

    Add ConfigUtil to KillionUtilities

--- src/main/java/co/killionrevival/killioncommons/KillionUtilities.java
+++ src/main/java/co/killionrevival/killioncommons/KillionUtilities.java
@@ -2,6 +2,7 @@ package co.killionrevival.killioncommons;
 
 import co.killionrevival.killioncommons.npc.NpcManager;
 import co.killionrevival.killioncommons.util.BroadcastUtil;
+import co.killionrevival.killioncommons.util.ConfigUtil;
 import co.killionrevival.killioncommons.util.ConsoleUtil;
 import co.killionrevival.killioncommons.util.MessageUtil;
 import co.killionrevival.killioncommons.util.SkinUtil;
@@ -14,11 +15,13 @@ public class KillionUtilities {
     private final ConsoleUtil consoleUtil;
     private final MessageUtil messageUtil;
     private final SkinUtil skinUtil;
+    private final ConfigUtil configUtil;
 
     public KillionUtilities(final Plugin plugin) {
         this.messageUtil = new MessageUtil(plugin);
         this.broadcastUtil = new BroadcastUtil(plugin);
         this.consoleUtil = new ConsoleUtil(plugin);
         this.skinUtil = new SkinUtil(plugin);
+        this.configUtil = new ConfigUtil(plugin);
     }
 }
