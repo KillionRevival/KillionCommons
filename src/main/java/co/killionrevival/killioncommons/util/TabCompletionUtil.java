ee680a4e7fa8b751affc34c2aa1b00cf6c515afb
default <mstrenk12@gmail.com>
2024-09-08 05:52:54 +0000

    Add some methods to help with TabComplete

---
+++ src/main/java/co/killionrevival/killioncommons/util/TabCompletionUtil.java
@@ -0,0 +1,49 @@
+package co.killionrevival.killioncommons.util;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class TabCompletionUtil {
+    public static List<String> getOnlinePlayersStartingWithMinusCurrentPlayer(Player currentPlayer, String input,
+            boolean caseInsensitive) {
+        if (caseInsensitive) {
+            return Bukkit.getOnlinePlayers().stream()
+                    .map(Player::getName)
+                    .filter(name -> name.toLowerCase().startsWith(input.toLowerCase()))
+                    .filter(name -> !name.toLowerCase().equals(currentPlayer.getName().toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return Bukkit.getOnlinePlayers().stream()
+                .map(Player::getName)
+                .filter(name -> name.startsWith(input))
+                .filter(name -> !name.equals(currentPlayer.getName()))
+                .collect(Collectors.toList());
+
+    }
+
+    public static List<String> getOnlinePlayersStartingWith(String input, boolean caseInsensitive) {
+        if (caseInsensitive) {
+            return Bukkit.getOnlinePlayers().stream()
+                    .map(Player::getName)
+                    .filter(name -> name.toLowerCase().startsWith(input.toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return Bukkit.getOnlinePlayers().stream()
+                .map(Player::getName)
+                .filter(name -> name.startsWith(input))
+                .collect(Collectors.toList());
+
+    }
+
+    public static List<String> filterListByPrefix(List<String> list, String prefix, boolean caseInsensitive) {
+        if (caseInsensitive) {
+            return list.stream().filter(name -> name.toLowerCase().startsWith(prefix.toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return list.stream().filter(name -> name.toLowerCase().startsWith(prefix.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+}
